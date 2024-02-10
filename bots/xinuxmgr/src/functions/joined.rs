8ce97ae12afebde0fdfb15923650bba3fc1cbfff
Sokhibjon Orzikulov <sakhib@orzklv.uz>
2024-02-11 04:01:26 +0500

    updated welcome message

--- bots/xinuxmgr/src/functions/joined.rs
+++ bots/xinuxmgr/src/functions/joined.rs
@@ -2,11 +2,20 @@ use teloxide::{prelude::*, types::*};
 
 use crate::utils::message::{delete_timer, Assistant};
 
-static TEXT: &str = "<b>Salom Administrator!</b>\n\n\
-Sizni ko'rib turganimdan bag'oyatda xursandman.
-Men Xinux Jamiyati tomonidan yaratilgan bot hisoblanib,
-Xinux Jamiyati foydalanuvchilari uchun foydali resurslarni yetkazish, saqlash va
-ularni saralash uchun xizmat qilaman.
+static TEXT: &str = "<b>Salom Administrator!</b>
+
+Sizni ko'rib turganimdan bag'oyatda xursandman. Men \
+Xinux Jamiyati tomonidan yaratilgan bot hisoblanib, \
+Xinux Jamiyati foydalanuvchilari uchun foydali resurslarni \
+yetkazish, saqlash va ularni saralash uchun xizmat qilaman. \
+Ushbu guruhda esa Linux ga oid istagan mavzuda gaplashish, \
+ma'lumot yoki tajriba ulashish mumkin.
+
+Agar siz bu yerlarga yordam axtarib kelgan bo'lsangiz, \
+<a href=\"https://t.me/xinuxuz/178646\">yordam kanali</a> \
+ga muroojat qilishingizni so'rab qolamiz. Hamda, qoidalarni \
+/rules buyrug'i orqali o'qib olish esingizdan chiqmasin. \
+Keyin yana adminlar jazolashsa hayron bo'lib yurmang!
 ";
 
 pub async fn trigger(bot: &Bot, msg: &Message) -> ResponseResult<()> {
