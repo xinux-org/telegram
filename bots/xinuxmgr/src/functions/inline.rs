a6ef73f7807a7632489a0d240cda251d06f7b1c1
Sokhibjon Orzikulov <sakhib@orzklv.uz>
2024-06-27 09:12:32 +0500

    covered everything to distro support

--- bots/xinuxmgr/src/functions/inline.rs
+++ bots/xinuxmgr/src/functions/inline.rs
@@ -39,41 +39,50 @@ pub async fn inline(
         0 => return_err_answer!(bot, q, "Qidirishni boshlang!", NO_INPUT),
         1 => return_err_answer!(bot, q, "Parametrlar yetarli emas!", NOT_ENOUGH),
         2 => {}
-        3.. => return_err_answer!(bot, q, "Parametrlar haddan ko'p!", TOO_MANY)
+        3.. => return_err_answer!(bot, q, "Parametrlar haddan ko'p!", TOO_MANY),
     }
 
-    let request = pkgs.search(parsed[1]).await;
+    match parsed[0] {
+        "arch" => {
+            let request = pkgs.search(parsed[1]).await;
 
-    let request: Vec<Data> = match request {
-        Ok(v) => v,
-        Err(_) => return_err_answer!(bot, q, "Xatolik yuz berdi!", INTERNAL_ERROR)
-    };
+            let request: Vec<Data> = match request {
+                Ok(v) => v,
+                Err(_) => return_err_answer!(bot, q, "Xatolik yuz berdi!", INTERNAL_ERROR),
+            };
 
-    let request: Vec<&Data> = request.iter().take(49).collect();
+            let request: Vec<&Data> = request.iter().take(49).collect();
 
-    if request.is_empty() {
-        return_err_answer!(bot, q, "Hech narsa topilmadi!", NOT_FOUND)
+            if request.is_empty() {
+                return_err_answer!(bot, q, "Hech narsa topilmadi!", NOT_FOUND)
+            }
+
+            let request: Vec<InlineQueryResult> = request
+                .iter()
+                .map(|d: &&Data| {
+                    InlineQueryResult::Article(
+                        InlineQueryResultArticle::new(
+                            uuid::Uuid::new_v4(),
+                            d.name.clone(),
+                            InputMessageContent::Text(
+                                InputMessageContentText::new(view_generate(d))
+                                    .parse_mode(ParseMode::Html)
+                                    .disable_web_page_preview(true),
+                            ),
+                        )
+                        .description(d.description.clone().unwrap())
+                        .reply_markup(kb_generate(d)),
+                    )
+                })
+                .collect();
+
+            bot.answer_inline_query(q.id, request).send().await?;
+        }
+        "nixos" => {
+            return_err_answer!(bot, q, "NixOS registri ustida ishlanmoqda!", NOT_FOUND)
+        }
+        _ => return_err_answer!(bot, q, "Noto'g'ri distributiv!", NOT_FOUND),
     }
 
-    let request: Vec<InlineQueryResult> = request
-        .iter()
-        .map(|d: &&Data| {
-            InlineQueryResult::Article(
-                InlineQueryResultArticle::new(
-                    uuid::Uuid::new_v4(),
-                    d.name.clone(),
-                    InputMessageContent::Text(
-                        InputMessageContentText::new(view_generate(d))
-                            .parse_mode(ParseMode::Html)
-                            .disable_web_page_preview(true),
-                    ),
-                )
-                .description(d.description.clone().unwrap())
-                .reply_markup(kb_generate(d)),
-            )
-        })
-        .collect();
-
-    bot.answer_inline_query(q.id, request).send().await?;
     Ok(())
 }
